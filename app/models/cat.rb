baa65df6fe6e56a39c2fbf509e55a708b3b03528
Frank-Proulx <frankproulx7@gmail.com>
2022-01-11 09:54:40 -0800

    add active storage to store cat pic urls for each cat
    Co-authored-by: Matt Casperson <mc.casperson@gmail.com>
    Co-authored-by: Drew Henderson <drewhendersonemail@gmail.com>
    Co-authored-by: Morgan Waites <morgan.waites@gmail.com>
    Co-authored-by: <isaacoverstreet3@gmail.com>
    Co-authored-by: Christopher Neal <christopher.m.neal@gmail.com>
    Co-authored-by: Isaac Overstreet <isaacoverstreet3@gmail.com>

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -2,4 +2,11 @@ class Cat < ApplicationRecord
   belongs_to :user
   validates :name, presence: true
   validates :breed, presence: true
+  has_one_attached :kitty_photo
+  before_save(:titleize_cat)
+
+  private
+    def titleize_cat
+      self.breed = self.breed.titleize
+    end
 end
